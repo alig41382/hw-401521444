3f151bb8104c600a543e9b02ca82a8dcaa0b1ba1
Ali <gholamiali41380@gmail.com>
2022-11-21 13:48:38 +0330

    first change in project

---
+++ project.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    printf("wrlcome to my game try to guess my favoriute number")
+
+    srand(time(NULL));
+    double number=rand()%10001+0;
+    double guessnumber;
+    printf("so tell me ur first guess")
+    scanf("%lf",&guessnumber);
+
+ while (guessnumber!=number)
+ {
+   
+   if (guessnumber<number)
+   {
+    printf("smaller\n");
+   }
+   else if (guessnumber>number)
+   {
+    printf("bigger\n");
+   }
+    printf("AHA You were wrong try another time");
+
+   scanf("%lf",&guessnumber);
+  
+ }
+ 
+ if (guessnumber==number)
+ {
+    printf("yourwin");
+ }
+ 
+}
